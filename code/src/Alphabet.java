f0cc2bfc1cf7fbd88915f5892f1cdae5cfee4ff9
QuestionMark001 <604661720@qq.com>
2021-12-15 22:20:42 +0800

    Update Alphabet.java

--- code/src/Alphabet.java
+++ code/src/Alphabet.java
@@ -3,6 +3,26 @@ public class Alphabet{
 	
 	public static void main(String[] args){
 		
-		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("请输入一个字符：");
+		char word = scan.next().charAt(0);
+
+		if(){
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+		scan.close();
 	}
 }
