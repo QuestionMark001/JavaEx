82b81bbf6c18fe2b696a08e2d04e6df056f938b1
QuestionMark001 <604661720@qq.com>
2021-12-15 22:20:49 +0800

    Update CylinderVol.java

--- code/src/CylinderVol.java
+++ code/src/CylinderVol.java
@@ -3,19 +3,18 @@ public class CylinderVol{
 
 	public static void main(String[] args){
 
-		double h,r,vol;//定义变量
 		Scanner scan = new Scanner(System.in);
 
 		System.out.println("计算圆柱体积");
 		
-		//输入
+		//定义变量并输入
 		System.out.print("请输入高：\n");
-		h = scan.nextDouble();
+		double h = scan.nextDouble();
 		System.out.print("请输入半径：\n");
-		r = scan.nextDouble();
+		double r = scan.nextDouble();
 		
 		//运算
-		vol = Math.PI*r*r*h;
+		double vol = Math.PI*r*r*h;
 
 		//输出
 		System.out.println("圆柱的体积为"+vol);
