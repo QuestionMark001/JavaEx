266aa9e86789b928f2ad2fa23e59e263c4c7aab7
QuestionMark001 <604661720@qq.com>
2021-12-17 03:21:21 +0800

    Update Circulate.java

--- code/src/Circulate.java
+++ code/src/Circulate.java
@@ -7,7 +7,7 @@ public class Circulate {
                 wordToken++;  //当100到999之间各数字之和为15时，wordToken自增1
                 System.out.printf("%-5d",num);  //以左对齐，5字符宽格式化输出变量num
 
-                if(wordToken == 10){  //当wordToken为10时换行，此时一行输出了10个num变量，
+                if(wordToken == 10){  //当wordToken为10时换行，此时一行输出了10个num变量
                     System.out.print("\n");  //换行
                     wordToken = 0;  //将0赋值给wordToken,开始下一行输出
                 }
