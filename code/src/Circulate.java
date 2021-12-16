7b2219aad1fe11c5308a7a4482b7d2663ede5596
QuestionMark001 <604661720@qq.com>
2021-12-17 03:02:52 +0800

    Update Circulate.java

--- code/src/Circulate.java
+++ code/src/Circulate.java
@@ -1,7 +1,20 @@
 public class Circulate {
     public static void main(String[] args){
         
-        int num,
+        int num,wordToken=0;//定义num用作输出的数字，wordToken用来控制变量num每行输出的个数
+        for(num = 100;num <= 999;num++){  //for循环从100遍历到999
+            if(num/100%10 + num/10%10 + num%10 == 15){
+                wordToken++;  //当100到999之间各数字之和为15时，wordToken自增1，并
+                System.out.printf();
+
+                if(wordToken == 10){
+                    System.out.print("\n");
+                    wordToken = 0;
+                }
+            }
+
+
+        }
 
     }
     
