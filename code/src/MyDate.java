4316267a7a0360a87aa5405e93b5f28260e35333
QuestionMark001 <604661720@qq.com>
2021-12-17 10:40:02 +0800

    Create MyDate.java

---
+++ code/src/MyDate.java
@@ -0,0 +1,121 @@
+import java.util.*;
+public class MyDate {
+    public static void main(String[] args)
+	{
+		//编写一个JAVA程序完成“输入一个合理的日期”的任务。
+		//要求：该程序的主方法中定义三个整型变量表示日期中的年、月、日的数值，要求月份值在1~12之间，
+		//日期值也在合理的范围内，比如不能出现2021年6月31日这样的日期。最后输出这个日期并有合适的文字说明。
+		Scanner in = new Scanner(System.in);
+		System.out.println("请输入年份：");
+		int year, month, day;
+		year = in.nextInt();
+		if( year % 4 == 0)
+		{//年份为闰年
+			System.out.println("请输入月份：");
+			int yeara = year;//yeara为闰年
+			month = in.nextInt();
+			if( month > 0 && month < 13) 
+			{//月份输入正确
+				
+				if(  month == 1 | month == 3 | month == 5 | month == 7 | month == 8 | month == 10 | month == 12) 
+				{
+					System.out.println("请输入日期：");
+					day = in.nextInt();
+					//月份是1，3，5，7，8，10，12的情况
+					if( day < 32 && day > 0)
+					{
+						System.out.println("输入正确！您输入的日期是："+yeara+"年"+month+"月"+day+"日");
+					}else 
+					{
+						System.out.println("日期输入错误！");
+					}
+				}else if(month == 2) 
+				{//月份是2月的情况
+					System.out.println("请输入日期：");
+					day = in.nextInt();
+					if (day < 30 && day > 0) 
+					{
+						System.out.println("输入正确！您输入的日期是："+yeara+"年"+month+"月"+day+"日");
+					}else 
+					{
+						System.out.println("日期输入错误！");
+					}	
+				}else if(  month == 4 | month == 6 | month == 9 | month ==11 )
+				{//月份是4，6，9，11的情况
+					System.out.println("请输入日期：");
+					day = in.nextInt();
+					if (day < 31 && day > 0) 
+					{
+						System.out.println("输入正确！您输入的日期是："+yeara+"年"+month+"月"+day+"日");
+					}else 
+					{
+						System.out.println("日期输入错误！");
+					}
+				}else 
+				{//月份是>31的情况下
+					System.out.println("日期输入错误！");
+				}
+			}else
+			{//月份输入错误
+				System.out.println("输入错误！");
+			}
+			
+		}else 
+		{//年份为平年
+			System.out.println("请输入月份：");
+			int yearb = year;//yearb为平年
+			month = in.nextInt();
+			if( month > 0 && month < 13) 
+			{//月份输入正确
+				
+				if(   month == 1 | month == 3 | month == 5 | month == 7 | month == 8 | month == 10 | month == 12) 
+				{
+					System.out.println("请输入日期：");
+					day = in.nextInt();
+					//月份是1，3，5，7，8，10，12的情况
+					if( day < 32 && day > 0)
+					{
+						System.out.println("输入正确！您输入的日期是："+yearb+"年"+month+"月"+day+"日");
+					}else 
+					{
+						System.out.println("日期输入错误！");
+					}
+				}else if(month == 2) 
+				{//月份是2月的情况
+					System.out.println("请输入日期：");
+					day = in.nextInt();
+					if (day < 29 && day > 0) 
+					{
+						System.out.println("输入正确！您输入的日期是："+yearb+"年"+month+"月"+day+"日");
+					}else 
+					{
+						System.out.println("日期输入错误！");
+					}	
+				}else if(  month == 4 | month == 6 | month == 9 | month ==11 )
+				{//月份是4，6，9，11的情况
+					System.out.println("请输入日期：");
+					day = in.nextInt();
+					if (day < 31 && day > 0) 
+					{
+						System.out.println("输入正确！您输入的日期是："+yearb+"年"+month+"月"+day+"日");
+					}else 
+					{
+						System.out.println("日期输入错误！");
+					}
+				}else 
+				{//月份是>31的情况下
+					System.out.println("日期输入错误！");
+				}
+			}else
+			{//月份输入错误
+				System.out.println("输入错误！");
+			}
+		}
+        //关闭对象close
+        in.close();
+		
+		
+
+	}
+    
+}
