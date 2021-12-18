e7b81f30e685c2bf77cb1f3c6aabfd4c9ad4e23e
QuestionMark Studio <53328516+QuestionMark001@users.noreply.github.com>
2021-12-18 19:31:03 +0800

    Add files via upload

---
+++ Java个人笔记(zh-cn)/Hello.java
@@ -0,0 +1,12 @@
+class Ha{
+	public static void main(String[] args) {
+		System.out.print("ppap");
+	}
+
+}
+class Da{
+	public static void main(String[] args) {
+		System.out.print("jk");
+	}
+	
+}
